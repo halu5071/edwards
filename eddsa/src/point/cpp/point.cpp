f06e4ee17a037cc747285a0e6963c98ae4dcaa8a
halu5071 <horiiortho5@gmail.com>
2018-11-16 13:55:39 +0900

    generate shared library.

--- eddsa/src/point/cpp/point.cpp
+++ eddsa/src/point/cpp/point.cpp
@@ -4,3 +4,7 @@
 int hello() {
   return 1;
 }
+
+JNIEXPORT jint JNICALL Java_io_moatwel_crypto_eddsa_ed25519_PointEd25519_hello(JNIEnv *env, jobject object) {
+  return;
+}
--- eddsa/src/point/headers/point.hpp
+++ eddsa/src/point/headers/point.hpp
@@ -1,6 +1,10 @@
 #ifndef POINT_H
 #define POINT_H
 
+#include <jni.h>
+
 int hello();
+
+JNIEXPORT jint JNICALL Java_io_moatwel_crypto_eddsa_ed25519_PointEd25519_hello(JNIEnv *env, jobject object);
 
 #endif
