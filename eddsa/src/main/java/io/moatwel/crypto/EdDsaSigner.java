ca70abb8d6ca47437b0e57ad632a162c5d5073f1
halu5071 <horiiortho5@gmail.com>
2018-11-23 21:49:33 +0900

    Remove unreached link on javadoc.

--- eddsa/src/main/java/io/moatwel/crypto/EdDsaSigner.java
+++ eddsa/src/main/java/io/moatwel/crypto/EdDsaSigner.java
@@ -4,8 +4,6 @@ package io.moatwel.crypto;
  * A interface represent signer for Edward-curve DSA.
  *
  * @author halu5071 (Yasunori Horii) at 2018/5/28
- * @see io.moatwel.crypto.eddsa.ed25519.Ed25519Signer
- * @see io.moatwel.crypto.eddsa.ed448.Ed448Signer
  */
 public interface EdDsaSigner {
 
